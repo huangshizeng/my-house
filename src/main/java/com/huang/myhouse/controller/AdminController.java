2c2dea00d983d5a1baee1a22c89b5714c1d94202
huangshizeng <shizeng101@163.com>
2019-04-17 12:38:28 +0800

    修改资源路径

---
+++ src/main/java/com/huang/myhouse/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.huang.myhouse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author hsz
+ */
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/center")
+    public String center() {
+        return "admin/center";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "admin/welcome";
+    }
+}
