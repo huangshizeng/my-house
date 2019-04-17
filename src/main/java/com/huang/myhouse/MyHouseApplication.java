7ab1ee1e5b419cbfed1ca16d48aad9890bc86624
huangshizeng <shizeng101@163.com>
2019-04-17 10:47:49 +0800

    初始化项目

---
+++ src/main/java/com/huang/myhouse/MyHouseApplication.java
@@ -0,0 +1,17 @@
+package com.huang.myhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hsz
+ */
+
+@SpringBootApplication
+public class MyHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHouseApplication.class, args);
+    }
+
+}
